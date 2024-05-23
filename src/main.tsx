fde7ec3613fb64d359835c602fa39a1ffc4d852c
aaryae <aaryaed@gmail.com>
2024-05-23 16:33:27 +0545

    bug fix: text chagnge

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import App from './App.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <ErrorBoundary fallback={<div className='h-[100vh] w-[100vh] flex justify-center items-center text-12xl'>OOPS, page not found👀</div>}>
+  <ErrorBoundary fallback={<div className='h-[100vh] w-[100vh] flex justify-center items-center text-8xl'>OOPS, page not found👀</div>}>
     <App />
   </ErrorBoundary>
 )
