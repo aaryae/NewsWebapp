730e676122aeb4a352b6ce429a5f9e40bf68d630
aaryae <aaryaed@gmail.com>
2024-05-23 16:31:49 +0545

    add: error boundary

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
-import page from "@assets/pnf/pagenotfound.png"
 import ReactDOM from 'react-dom/client'
 import { ErrorBoundary } from 'react-error-boundary'
 import App from './App.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <ErrorBoundary fallback={<div className='h-[100vh] w-[100vh] flex justify-center items-center text-4xl'><img src={page} alt="page not found" className='h-[100vh] w-[100vh] object-cover ' /></div>}>
+  <ErrorBoundary fallback={<div className='h-[100vh] w-[100vh] flex justify-center items-center text-12xl'>OOPS, page not found👀</div>}>
     <App />
   </ErrorBoundary>
 )
