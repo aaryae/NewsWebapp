f7a2b7016201887e639aac8ee27ecff7fb5df137
aaryae <aaryaed@gmail.com>
2024-05-23 12:26:10 +0545

    bug : error boundary

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import App from './App.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <ErrorBoundary fallback={<div>something went wrong</div>}>
+  <ErrorBoundary fallback={<div className='h-full flex justify-center items-center text-4xl'>something went wrong</div>}>
 
     <App />
   </ErrorBoundary>
